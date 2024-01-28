00916da7de50ff491b51fb387c52029e74d75163
Ishi <chitrda112@gmail.com>
2024-01-29 05:14:16 +0700

    initial commit

---
+++ src/test/java/com/yui/YuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
