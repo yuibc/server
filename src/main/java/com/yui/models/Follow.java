58a1848d7d766bab35f6ebeb830173193e2e5d8a
ishi <berugen1st@gmail.com>
2024-03-12 15:55:13 +0700

    feat: update mapping 1-N to models

--- src/main/java/com/yui/models/Follow.java
+++ src/main/java/com/yui/models/Follow.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,7 +18,11 @@ public class Follow {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
     private User user;
+
     private User followingUser;
     private Date createdAt;
 }
--- src/main/java/com/yui/models/User.java
+++ src/main/java/com/yui/models/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -23,6 +24,13 @@ public class User {
     private String walletAddress;
     private Boolean isAdmin;
     private Date createdAt;
+    
+    @OneToMany(mappedBy="user")
     private List<Artwork> artworks;
+
+    @OneToMany(mappedBy="user")
     private List<Comment> comments;
+
+    @OneToMany(mappedBy="user")
+    private List<Follow> follows;
 }
