3fb2a2713b370f2db17236e9146388b7f5104d2e
ishi <berugen1st@gmail.com>
2024-03-12 16:53:51 +0700

    Revert "feat: define @Column in User explicitly"
    This reverts commit 1b7464359c49d3783ec89381c48f0975a8fef78a.

--- src/main/java/com/yui/models/User.java
+++ src/main/java/com/yui/models/User.java
@@ -3,7 +3,6 @@ package com.yui.models;
 import java.sql.Timestamp;
 import java.util.List;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,25 +17,12 @@ import lombok.Data;
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id", columnDefinition="integer")
     private Long id;
-
-    @Column(name="email", columnDefinition="varchar(255)", unique = true)
     private String email;
-
-    @Column(name="display_name", columnDefinition="varchar(50)")
     private String displayName;
-
-    @Column(name="password", columnDefinition="varchar(50)")
     private String password;
-
-    @Column(name="wallet_address", columnDefinition="varchar(MAX)")
     private String walletAddress;
-
-    @Column(name="is_admin", columnDefinition="bit")
     private Boolean isAdmin;
-
-    @Column(name="created_at", columnDefinition="timestamp")
     private Timestamp createdAt;
     
     @OneToMany(mappedBy="user")
