0514426d9d6acd076f7e935105dbe5a12fa50a36
ishi <berugen1st@gmail.com>
2024-03-17 19:46:22 +0700

    feat: include providers in main function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
 import { AppDataSource } from './data-source';
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import {
+    ArtworkProvider,
+    AuthProvider,
+    CartProvider,
+    CategoryProvider,
+    CommentProvider,
+    FeedbackProvider,
+    FollowProvider,
+    NFTProvider,
+    UserProvider,
+} from './providers';
 
 function main() {
     dotenv.config();
     const app: Express = express();
     AppDataSource.initialize()
         .then(async () => {
+            const router = express.Router();
+            app.use('/api/v1', router);
+            app.use(AuthProvider(router));
+            app.use(UserProvider(router));
+            app.use(ArtworkProvider(router));
+            app.use(CartProvider(router));
+            app.use(CategoryProvider(router));
+            app.use(FollowProvider(router));
+            app.use(CommentProvider(router));
+            app.use(NFTProvider(router));
+            app.use(FeedbackProvider(router));
             app.listen(4000, () =>
                 console.log('Server initializing at port: 4000'),
             );
