08327d407bb4a5979c61ac8257c0c155299f6ed2
ishi <berugen1st@gmail.com>
2024-03-17 12:29:00 +0700

    feat: config auth request in config

--- src/main/java/com/yui/YuiConfiguration.java
+++ src/main/java/com/yui/YuiConfiguration.java
@@ -2,16 +2,22 @@ package com.yui;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.yui.providers.AuthProvider;
 
 /**
  *
@@ -20,6 +26,13 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class YuiConfiguration {
+
+    private final AuthProvider authProvider;
+
+    @Autowired
+    public YuiConfiguration(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,7 +54,16 @@ public class YuiConfiguration {
         http
             .httpBasic(basic -> basic.disable())
             .formLogin(form -> form.disable())
-            .logout(log -> log.disable());
+            .logout(log -> log.disable())
+            .authorizeHttpRequests(
+                h -> h
+                    .requestMatchers("/api/v1/auth/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
+                    .anyRequest()
+                    .authenticated()
+            );
+        http.addFilterBefore(new JwtAuthenticationFilter(authProvider), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
