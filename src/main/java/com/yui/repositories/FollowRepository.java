14c1b844e29864a4c02f7b1a25365e87b868a229
ishi <berugen1st@gmail.com>
2024-03-17 10:47:04 +0700

    feat: add findByUser in Follow repo

--- src/main/java/com/yui/repositories/FollowRepository.java
+++ src/main/java/com/yui/repositories/FollowRepository.java
@@ -1,12 +1,16 @@
 package com.yui.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yui.models.Follow;
+import com.yui.models.User;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    public List<Follow> findByUser(User user);
 }
 
 
