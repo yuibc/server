ee32cf360ad58d51bc2bb9fae43555b199c5f40e
ishi <berugen1st@gmail.com>
2024-03-18 05:40:25 +0700

    feat: add untested auth middlewares

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+import { isMatch } from '../helpers';
+import { userRepository as repo } from '../repositories';
+
+export const verifyTraditional = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const { email, password } = req.body;
+
+    const user = await repo.findOneBy({ email, password });
+
+    if (!user) {
+        throw new Error('User does not exist!');
+    }
+
+    const isUserMatch = await isMatch(
+        password as string,
+        user.password as string,
+    );
+
+    if (!isUserMatch) {
+        throw new Error('Password does not match!');
+    }
+
+    res.locals.userId = user.id;
+    res.locals.email = user.email;
+    next();
+};
+
+export const verifyWalletAddress = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const { walletAddress } = req.body;
+    const user = await repo.findOneBy({ walletAddress });
+    if (!user) {
+        throw new Error('User does not exist!');
+    }
+    res.locals.walletAddress = walletAddress;
+    next();
+};
