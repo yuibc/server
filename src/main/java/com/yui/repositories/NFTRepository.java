7dfc3957c2d2091b8296433f684dd6f972763c62
ishi <berugen1st@gmail.com>
2024-03-17 10:34:55 +0700

    feat: add findByUser in NFT repo

--- src/main/java/com/yui/repositories/NFTRepository.java
+++ src/main/java/com/yui/repositories/NFTRepository.java
@@ -1,11 +1,14 @@
 package com.yui.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yui.models.NFT;
+import com.yui.models.User;
 
 @Repository
 public interface NFTRepository extends JpaRepository<NFT, Long> {
-    
+    public List<NFT> findByUser(User user);
 }
