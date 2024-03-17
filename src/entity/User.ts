9aed6500ba382eb264e55d82579b4796ebe2cbe6
ishi <berugen1st@gmail.com>
2024-03-17 18:12:22 +0700

    feat: discard User model template and fill in

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,48 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    BaseEntity,
+    Timestamp,
+    OneToMany,
+} from 'typeorm';
+import { Artwork, Cart, Comment, Follow } from '.';
 
 @Entity()
-export class User {
-
+export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
     @Column()
-    firstName: string
+    email: string;
 
     @Column()
-    lastName: string
+    displayName: string;
 
     @Column()
-    age: number
+    password: string;
 
+    @Column()
+    walletAddress: string;
+
+    @Column()
+    isAdmin: boolean;
+
+    @Column()
+    createdAt: Timestamp;
+
+    @OneToMany(() => Artwork, (artwork) => artwork.user)
+    artworks: Artwork[];
+
+    @OneToMany(() => Follow, (follow) => follow.user)
+    follows: Follow[];
+
+    @OneToMany(() => Follow, (follow) => follow.followingUser)
+    followUsers: Follow[];
+
+    @OneToMany(() => Comment, (comment) => comment.user)
+    comments: Comment[];
+
+    @OneToMany(() => Cart, (cart) => cart.user)
+    carts: Cart[];
 }
