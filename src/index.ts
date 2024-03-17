1d1d1f694889dc8cab53289d1407a40d2c632c24
ishi <berugen1st@gmail.com>
2024-03-17 18:53:17 +0700

    feat: remove all boilerplate and config server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,14 @@
 import { AppDataSource } from './data-source';
-import { User } from './entity/User';
 import express, { Express } from 'express';
+import dotenv from 'dotenv';
 
 function main() {
+    dotenv.config();
     const app: Express = express();
     AppDataSource.initialize()
         .then(async () => {
-            console.log('Inserting a new user into the database...');
-            const user = new User();
-            user.firstName = 'Timber';
-            user.lastName = 'Saw';
-            user.age = 25;
-            await AppDataSource.manager.save(user);
-            console.log('Saved a new user with id: ' + user.id);
-
-            console.log('Loading users from the database...');
-            const users = await AppDataSource.manager.find(User);
-            console.log('Loaded users: ', users);
-
-            app.listen(4000);
-            console.log(
-                'Here you can setup and run express / fastify / any other framework.',
+            app.listen(4000, () =>
+                console.log('Server initializing at port: 4000'),
             );
         })
         .catch((error) => console.log(error));
