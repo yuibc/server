d496b8de0774e9c44276b157d25bcb1025787012
ishi <berugen1st@gmail.com>
2024-03-24 16:41:23 +0700

    feat: discard traditional auth, middleware and password in user entity

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,6 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Artwork, Cart, Comment, Follow } from '.';
-import { hashPassword } from '../helpers';
 
 @Entity()
 export class User {
@@ -12,9 +11,6 @@ export class User {
 
     @Column()
     displayName: string;
-
-    @Column()
-    password: string;
 
     @Column({ nullable: true })
     walletAddress: string;
@@ -39,8 +35,4 @@ export class User {
 
     @OneToMany(() => Cart, (cart) => cart.user)
     carts: Cart[];
-
-    async setPassword(password: string) {
-        this.password = await hashPassword(password);
-    }
 }
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,35 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
-import { compare } from '../helpers';
 import { userRepository as repo } from '../repositories';
 
-export const verifyTraditional = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
-    const { email, password } = req.body;
+// export const verifyTraditional = async (
+//     req: Request,
+//     res: Response,
+//     next: NextFunction,
+// ) => {
+//     const { email, password } = req.body;
 
-    const user = await repo.findOneBy({
-        email,
-    });
+//     const user = await repo.findOneBy({
+//         email,
+//     });
 
-    if (!user) {
-        throw new Error('User does not exist!');
-    }
+//     if (!user) {
+//         throw new Error('User does not exist!');
+//     }
 
-    const isUserMatch = await compare(
-        password as string,
-        user.password as string,
-    );
+//     const isUserMatch = await compare(
+//         password as string,
+//         user.password as string,
+//     );
 
-    if (!isUserMatch) {
-        throw new Error('Password does not match!');
-    }
+//     if (!isUserMatch) {
+//         throw new Error('Password does not match!');
+//     }
 
-    res.locals.userId = user.id;
-    res.locals.email = user.email;
-    next();
-};
+//     res.locals.userId = user.id;
+//     res.locals.email = user.email;
+//     next();
+// };
 
 export const verifyWalletAddress = async (
     req: Request,
