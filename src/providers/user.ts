99765b2675a5db3058e6cb517e983d364f674756
ishi <berugen1st@gmail.com>
2024-03-31 20:19:03 +0700

    feat: add method find user by display name

--- src/providers/user.ts
+++ src/providers/user.ts
@@ -6,6 +6,20 @@ import { userRepository as repo } from '../repositories/user';
 export const UserProvider = (router: Router) => {
     router.get('/users', async () => await repo.find());
 
+    router.get('/:displayName/user', async (req: Request, res: Response) => {
+        try {
+            const { displayName } = req.params;
+            const user = await repo.findOne({
+                where: { displayName },
+                select: ['walletAddress'],
+            });
+            res.status(200).send(user);
+        } catch (e) {
+            console.log(e);
+            res.status(500).send(ResponseMessage.SERVER_ERROR);
+        }
+    });
+
     router.get('/users/:id', async (req: Request, res: Response) => {
         try {
             const { id } = req.params;
